3db50237bf794de4d0a08a166ebf1ff46be2721f
marvinguo <overearth@gmail.com>
2022-02-21 18:40:07 +0800

    cargo expand test

---
+++ examples/tokio_expand.rs
@@ -0,0 +1,6 @@
+// async fn in trait is not stable
+// try use async_trait crate instead to test
+// check https://smallcultfollowing.com/babysteps/blog/2019/10/26/async-fn-in-traits-are-hard/
+
+fn main() {
+}
