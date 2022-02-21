309735dca0b93a521ef98943d3787a8a5ebca64b
marvinguo <overearth@gmail.com>
2022-02-22 07:37:23 +0800

    mix async trait with tokio runtime

--- examples/async_trait.rs
+++ examples/async_trait.rs
@@ -1,7 +1,33 @@
+// async fn in trait is not stable
+// try use async_trait crate instead to test
+// check https://smallcultfollowing.com/babysteps/blog/2019/10/26/async-fn-in-traits-are-hard/
 
+use tokio::runtime::Runtime;
+use async_trait::async_trait;
 
-fn main() {
-    let a = 10;
-    // the check_add returns Option comparing to +
-    let b = a.checked_add(100).unwrap();
+#[async_trait]
+trait Engine {
+    type Output;
+    async fn run(&self) -> Result<Self::Output, ()>;
+}
+
+struct Simple;
+
+#[async_trait]
+impl Engine for Simple {
+    type Output = i32;
+
+    async fn run(&self) -> Result<Self::Output, ()> {
+        println!("run");
+        Ok(123)
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let runtime = Runtime::new()?;
+    let simple = Simple;
+    let future = simple.run();
+    let result = runtime.block_on(future);
+    println!("result:{:?}", result.unwrap());
+    Ok(())
 }
